ded1084cce79d19f23bf8653361f17d8d682a1af
Byron Murgatroyd <byron@omanom.com>
2026-07-07 13:31:01 +0100

    Removing old license headers

--- cli/components/cmd/utils.go
+++ cli/components/cmd/utils.go
@@ -1,8 +1,3 @@
-/* Copyright (C) Byron Murgatroyd - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Byron Murgatroyd <byron.murgatroyd@appdef.io>, 2022
- */
 package cmd
 
 import (
